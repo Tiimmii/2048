5aecb9fb6a3db71a398b6e10b73a3af8783efa8a
Timmi <danieloluwoleoluwatimileyin@gmail.com>
2023-05-18 15:22:48 +0100

    generat 2 randomly

--- 2048.js
+++ 2048.js
@@ -2,14 +2,25 @@ document.addEventListener('DOMContentLoaded', ()=>{
     let displarGrid = document.querySelector('.grid')
     let score = document.getElementById('score-value')
     let width = 16
+    let squares = []
 
     function drawBox(){
-        for(let i; i<width; i++){
-            let box = document.createElement('div')
-            box.innerHTML = 0
-            displarGrid.appendChild(box)
+        for(let i=0; i<width; i++){
+            let square = document.createElement('div')
+            square.innerHTML = 0
+            displarGrid.appendChild(square)
+            squares.push(square)
         }
+        generate()
+        generate()
     }
-
     drawBox()
+    function generate(){
+        let rand_num = Math.floor(Math.random() * squares.length)
+        if(squares[rand_num].innerHTML==0){
+            squares[rand_num].innerHTML=2
+        }
+        else generate()
+        console.log(squares)
+    }
 })
