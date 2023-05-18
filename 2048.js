c161b899580df601392e35074cb07d2c1eff511e
Timmi <danieloluwoleoluwatimileyin@gmail.com>
2023-05-18 14:56:16 +0100

    js

---
+++ 2048.js
@@ -0,0 +1,11 @@
+document.addEventListener('DOMContentLoaded', ()=>{
+    let displarGrid = document.querySelector('.grid')
+    let score = document.getElementById('score-value')
+    let width = 16
+
+    function drawBox(){
+        for(let i; i<width; i++){
+            
+        }
+    }
+})
